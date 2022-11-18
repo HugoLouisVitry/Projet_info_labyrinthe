14c89c7df9778bf52d53c64c67cd1fd36923cf11
Vitry Hugo <113689285+HugoLouisVitry@users.noreply.github.com>
2022-11-18 21:49:19 +0100

    Esquisses des instruction pour le code

---
+++ labyrinthe.py
@@ -0,0 +1,53 @@
+111E1111111111111
+10000000001010001
+11111110101011011
+10001000100000001
+11101110101010101
+10000000101010101
+11111111101111101
+10101010000010101
+10101011111010111
+10001010100000101
+11101000111011101
+10001010000000001
+10111011111011101
+10000000001010101
+10101010101010001   
+11111S11111111111
+
+
+
+############################
+# ALGORITHME PAR FONCTIONS
+############################
+# récupère le fichier texte
+
+# lire le fichier 
+
+
+# Pour chaque carractère on le met dans une liste 
+# On crée une matrice qui sera le labyrinthe dont 
+# les lignes sont les listes
+
+
+# identifier les noeud 
+# on regarde tout les zéros ,
+# on regarde le nombre de zéro qui entoure ce zéro ,
+# 1 : cul de sac
+# 2: c'est juste un chemin
+# 3 ou 4 : c'est un noeud
+
+
+
+
+# interface graphique (utiliser tkinter ou turtle)
+# mettre des point noirs au mur et blanc aux chemin
+# symbole début arrivé 
+
+
+
+
+
+
+
+
