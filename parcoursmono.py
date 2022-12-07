80c0fb042463f989fcb5d041ec22ec7208b4cbd8
Vitry Hugo <113689285+HugoLouisVitry@users.noreply.github.com>
2022-12-07 22:01:14 +0100

    ça marche et c'est propre
    Déplacement du heap dans une autre biblio
    le " if main" fait une démonstration

--- parcoursmono.py
+++ parcoursmono.py
@@ -1,57 +1,10 @@
 
 import numpy as np
 import math as m
-import heapq as hq
 
 import Maze_converter
+import pimped_heap
 
-def heappop_modified(heap):
-    """Pop the smallest item off the heap, maintaining the heap invariant."""
-    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
-    if heap:
-        returnitem = heap[0]
-        heap[0] = lastelt
-        _siftup_modified(heap, 0)
-        return returnitem
-    return lastelt
-def _siftup_modified(heap, pos):
-    endpos = len(heap)
-    startpos = pos
-    newitem = heap[pos]
-    # Bubble up the smaller child until hitting a leaf.
-    childpos = 2*pos + 1    # leftmost child position
-    while childpos < endpos:
-        # Set childpos to index of smaller child.
-        rightpos = childpos + 1
-        if rightpos < endpos and not heap[childpos].dist < heap[rightpos].dist:
-            childpos = rightpos
-        # Move the smaller child up.
-        heap[pos] = heap[childpos]
-        pos = childpos
-        childpos = 2*pos + 1
-    # The leaf at pos is empty now.  Put newitem there, and bubble it up
-    # to its final resting place (by sifting its parents down).
-    heap[pos] = newitem
-    _siftdown_modified(heap, startpos, pos)   
-
-def heappush_modified(heap, item):
-    """Push item onto heap, maintaining the heap invariant."""
-    heap.append(item)
-    _siftdown_modified(heap, 0, len(heap)-1)
-
-def _siftdown_modified(heap, startpos, pos):
-    newitem = heap[pos]
-    # Follow the path to the root, moving parents down until finding a place
-    # newitem fits.
-    while pos > startpos:
-        parentpos = (pos - 1) >> 1
-        parent = heap[parentpos]
-        if newitem.dist < parent.dist:
-            heap[pos] = parent
-            pos = parentpos
-            continue
-        break
-    heap[pos] = newitem
 
 
 class NotFound (Exception):
@@ -117,35 +70,34 @@ def parcours_mono (nodes , id_entree , id_sortie ) :
     Graphe_Labyrinthe = WGraph () 
     for node in nodes : 
         #print(nodes[node])
-         Graphe_Labyrinthe.add_node(nodes[node])
-         #print(Graphe_Labyrinthe.nodes)
-         nodes[node].dist = m.inf
-         nodes[node].pred = None
-         nodes[node].closed = False
+        Graphe_Labyrinthe.add_node(nodes[node])
+        #print(Graphe_Labyrinthe.nodes)
+        nodes[node].dist = m.inf
+        nodes[node].pred = None
+        nodes[node].closed = False
     #print(Graphe_Labyrinthe.nodes)
     Graphe_Labyrinthe.nodes[id_entree].dist = 0
     heap =[ Graphe_Labyrinthe.nodes[id_entree]]
 
     while heap :
-         node = heappop_modified(heap)
-         node.closed = True
-         if node.id == id_sortie :
-            print(Graphe_Labyrinthe.traverse( id_sortie ),node.dist)
-            return ( node.dist , Graphe_Labyrinthe.traverse( id_sortie ) )
-         for voisin_id in node.neightboors_dist:
-             print(node,voisin_id)
-             voisin_dist=node.neightboors_dist[voisin_id]
-             if not Graphe_Labyrinthe.nodes[voisin_id].closed :
-                dist = node.dist + voisin_dist
-                v = Graphe_Labyrinthe.nodes[ voisin_id ]
-                vpred = v.pred
-
-                if v.dist > dist :
-                   v.dist = dist
-                   v.pred = node.id
-                if vpred is None :
-                    heappush_modified ( heap , v )
-#                else :
+        node = pimped_heap.heappop(heap)
+        node.closed = True
+        if node.id == id_sortie :
+           #print(Graphe_Labyrinthe.traverse( id_sortie ),node.dist)
+           return (Graphe_Labyrinthe.traverse( id_sortie ), node.dist )
+        for voisin_id in node.neightboors_dist:
+            #print(node,voisin_id)
+            voisin_dist=node.neightboors_dist[voisin_id]
+            if not Graphe_Labyrinthe.nodes[voisin_id].closed :
+               dist = node.dist + voisin_dist
+               v = Graphe_Labyrinthe.nodes[ voisin_id ]
+               vpred = v.pred
+               if v.dist > dist :
+                  v.dist = dist
+                  v.pred = node.id
+               if vpred is None :
+                   pimped_heap.heappush( heap , v )
+#               else :
 #                    hq.heapdecrease( heap , voisin_id )
 
 
@@ -153,13 +105,17 @@ def parcours_mono (nodes , id_entree , id_sortie ) :
 if __name__ == '__main__':
     Maze_file= "lab.txt"
     Graphic_maze,nodes = Maze_converter.get_all(Maze_file)
-    print(nodes)
+    #print(nodes)
     Maze_converter.graphics(Graphic_maze)
 
     for NODE in nodes:
-        print(nodes[NODE].neightboors_dist)
+        #print(str(NODE)+"->",nodes[NODE].neightboors_dist,"\n")
         if nodes[NODE].entry:
             id_entree = nodes[NODE].id
         if nodes[NODE].exit:
             id_sortie = nodes[NODE].id
-    parcours_mono(nodes,id_entree,id_sortie)
+    chemin=parcours_mono(nodes,id_entree,id_sortie)[0]
+    Maze_converter.update_final(Graphic_maze,chemin)
+    Maze_converter.graphics(Graphic_maze)
+
+    
