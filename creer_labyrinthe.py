8ca3d681709732481b0bfa693c082394ec262fd6
Vitry Hugo <113689285+HugoLouisVitry@users.noreply.github.com>
2022-12-14 09:50:31 +0100

    version exportable
    modification des fonctions pour qu'elles utilisent un labyrinthe global et non local. Il suffit maintenant d'appeller la fonction  "laby"  qui renvoie un labyrinthe sous format matriciel.

--- creer_labyrinthe.py
+++ creer_labyrinthe.py
@@ -1,70 +1,68 @@
 import numpy as np
 import random
-
-#0 chemin
-#1 mur
-#2 entree
-#3 sortie
-#4 ni ni(pour initialiser, les chemins non visitées...)
+import maze_converter
+import parcoursmono
+CHEMIN = 0
+MUR = 1
+ENTREE = 2
+SORTIE = 3
+NI_NI = 4  #ni ni(pour initialiser, les chemins non visitées...)
 
 def lab0(lignes, colonnes): #matrice initiale
     return np.random.randint(4, 5, (lignes, colonnes))
-    
 
-
-
-def chemins_voisins(mur):
+def chemins_voisins(mur,lab):
     nb = 0 #nb des chenins voisins
-    if (lab[mur[0]-1][mur[1]] == 0):
+    if (lab[mur[0]-1][mur[1]] == CHEMIN):
         nb += 1
-    if (lab[mur[0]+1][mur[1]] == 0):
+    if (lab[mur[0]+1][mur[1]] == CHEMIN):
         nb += 1
-    if (lab[mur[0]][mur[1]-1] == 0):
+    if (lab[mur[0]][mur[1]-1] == CHEMIN):
         nb +=1
-    if (lab[mur[0]][mur[1]+1] == 0):
+    if (lab[mur[0]][mur[1]+1] == CHEMIN):
         nb += 1
     return nb
             
-def make_murs(lignes, colonnes):
+def make_murs(lignes, colonnes,lab):
     for i in range(lignes):
         for j in range(colonnes):
-            if (lab[i][j] == 4):
-                lab[i][j] = 1
+            if (lab[i][j] == NI_NI):
+                lab[i][j] = MUR
                 
-def entree_sortie(lignes, colonnes):
+def entree_sortie(lignes, colonnes,lab):
     for i in range(lignes):
-        if (lab[1][i] == 0):
-            lab[0][i] = 2
+        if (lab[1][i] == CHEMIN):
+            lab[0][i] = ENTREE
             break
     for i in range(colonnes-1, 0, -1):
-        if (lab[lignes-2][i] == 0):
-            lab[lignes-1][i] = 3
+        if (lab[lignes-2][i] == CHEMIN):
+            lab[lignes-1][i] = SORTIE
             break
 def creer_lab(lignes, colonnes):
-            
+    lab = lab0(lignes,colonnes)
     ligne1 = int(random.random()*lignes)
     colonne1 = int(random.random()*colonnes)
     
-    if ligne1 == 0:
+    if ligne1 == CHEMIN:
         ligne1 += 1
     if ligne1 == lignes-1:
         ligne1 -= 1
-    if colonne1 == 0:
+    if colonne1 == CHEMIN:
         colonne1 += 1
     if colonne1 == colonnes-1:
         colonne1 -= 1
         
-    lab[ligne1][colonne1] = 0
+    lab[ligne1][colonne1] = CHEMIN
     murs = []
     murs.append([ligne1-1, colonne1])
     murs.append([ligne1, colonne1-1])
     murs.append([ligne1, colonne1+1])
     murs.append([ligne1+1, colonne1])
     
-    lab[ligne1-1][colonne1] = 1
-    lab[ligne1][colonne1-1] = 1
-    lab[ligne1][colonne1+1] = 1
-    lab[ligne1+1][colonne1] = 1
+    lab[ligne1-1][colonne1] = MUR
+    lab[ligne1][colonne1-1] = MUR
+    lab[ligne1][colonne1+1] = MUR
+    lab[ligne1+1][colonne1] = MUR
     
     
     def delete_wall(mur):
@@ -75,27 +73,27 @@ def creer_lab(lignes, colonnes):
         
         mur= murs[int(random.random()*len(murs))-1] #random wall
         
-        if mur[1] != 0:
-            if lab[mur[0]][mur[1]-1] == 4 and lab[mur[0]][mur[1]+1] == 0:
-                nb_chemins = chemins_voisins(mur)
+        if mur[1] != CHEMIN:
+            if lab[mur[0]][mur[1]-1] == NI_NI and lab[mur[0]][mur[1]+1] == CHEMIN:
+                nb_chemins = chemins_voisins(mur,lab)
                 if nb_chemins < 2:
-                    lab[mur[0]][mur[1]] = 0
+                    lab[mur[0]][mur[1]] = CHEMIN
                     
-                    if (mur[0] != 0):
-                        if (lab[mur[0]-1][mur[1]] != 0):
-                            lab[mur[0]-1][mur[1]] = 1
+                    if (mur[0] != CHEMIN):
+                        if (lab[mur[0]-1][mur[1]] != CHEMIN):
+                            lab[mur[0]-1][mur[1]] = MUR
                             if ([mur[0]-1, mur[1]] not in murs):
                                 murs.append([mur[0]-1, mur[1]])
                     if (mur[0] != lignes-1):
-                        if (lab[mur[0]+1][mur[1]] != 0):
-                            lab[mur[0]+1][mur[1]] = 1
+                        if (lab[mur[0]+1][mur[1]] != CHEMIN):
+                            lab[mur[0]+1][mur[1]] = MUR
                         if ([mur[0]+1, mur[1]] not in murs):
                             murs.append([mur[0]+1, mur[1]])
     
     				# Leftmost cell
-                    if (mur[1] != 0):	
-                        if (lab[mur[0]][mur[1]-1] != 0):
-                            lab[mur[0]][mur[1]-1] = 1
+                    if (mur[1] != CHEMIN):	
+                        if (lab[mur[0]][mur[1]-1] != CHEMIN):
+                            lab[mur[0]][mur[1]-1] = MUR
                         if ([mur[0], mur[1]-1] not in murs):
                             murs.append([mur[0], mur[1]-1])
     			
@@ -104,25 +102,25 @@ def creer_lab(lignes, colonnes):
                 delete_wall(mur)
                 
             
-        if mur[0] != 0:
-            if lab[mur[0]-1][mur[1]] == 4 and  lab[mur[0]+1][mur[1]] == 0:
-                nb_chemins = chemins_voisins(mur)
+        if mur[0] != CHEMIN:
+            if lab[mur[0]-1][mur[1]] == NI_NI and  lab[mur[0]+1][mur[1]] == CHEMIN:
+                nb_chemins = chemins_voisins(mur,lab)
                 if nb_chemins < 2:
-                    lab[mur[0]][mur[1]] = 0
+                    lab[mur[0]][mur[1]] = CHEMIN
                     
-                    if (mur[0] != 0):
-                        if (lab[mur[0]-1][mur[1]] != 0):
-                            lab[mur[0]-1][mur[1]] = 1
+                    if (mur[0] != CHEMIN):
+                        if (lab[mur[0]-1][mur[1]] != CHEMIN):
+                            lab[mur[0]-1][mur[1]] = MUR
                         if ([mur[0]-1, mur[1]] not in murs):
                             murs.append([mur[0]-1, mur[1]])
-                    if (mur[1] != 0):
-                        if (lab[mur[0]][mur[1]-1] != 0):
-                            lab[mur[0]][mur[1]-1] = 1
+                    if (mur[1] != CHEMIN):
+                        if (lab[mur[0]][mur[1]-1] != CHEMIN):
+                            lab[mur[0]][mur[1]-1] = MUR
                         if ([mur[0], mur[1]-1] not in murs):
                             murs.append([mur[0], mur[1]-1])
                     if (mur[1] != colonnes-1):
-                        if (lab[mur[0]][mur[1]+1] != 0):
-                            lab[mur[0]][mur[1]+1] = 1 
+                        if (lab[mur[0]][mur[1]+1] != CHEMIN):
+                            lab[mur[0]][mur[1]+1] = MUR 
                         if ([mur[0], mur[1]+1] not in murs):
                             murs.append([mur[0], mur[1]+1])
                                 
@@ -132,24 +130,24 @@ def creer_lab(lignes, colonnes):
     
                 
         if mur[1] != colonnes -1:
-            if lab[mur[0]][mur[1]-1] == 0 and lab[mur[0]][mur[1]+1] == 4:
-                nb_chemins = chemins_voisins(mur)
+            if lab[mur[0]][mur[1]-1] == CHEMIN and lab[mur[0]][mur[1]+1] == NI_NI:
+                nb_chemins = chemins_voisins(mur,lab)
                 if nb_chemins < 2:
-                    lab[mur[0]][mur[1]] = 0
+                    lab[mur[0]][mur[1]] = CHEMIN
                                 
                     if (mur[1] != colonnes-1):
-                        if (lab[mur[0]][mur[1]+1] != 0):
-                            lab[mur[0]][mur[1]+1] = 1
+                        if (lab[mur[0]][mur[1]+1] != CHEMIN):
+                            lab[mur[0]][mur[1]+1] = MUR
                         if ([mur[0], mur[1]+1] not in murs):
                             murs.append([mur[0], mur[1]+1])
                     if (mur[0] != lignes-1):
-                        if (lab[mur[0]+1][mur[1]] != 0):
-                            lab[mur[0]+1][mur[1]] = 1
+                        if (lab[mur[0]+1][mur[1]] != CHEMIN):
+                            lab[mur[0]+1][mur[1]] = MUR
                         if ([mur[0]+1, mur[1]] not in murs):
                             murs.append([mur[0]+1, mur[1]])
-                    if (mur[0] != 0):	
-                        if (lab[mur[0]-1][mur[1]] != 0):
-                            lab[mur[0]-1][mur[1]] = 1
+                    if (mur[0] != CHEMIN):	
+                        if (lab[mur[0]-1][mur[1]] != CHEMIN):
+                            lab[mur[0]-1][mur[1]] = MUR
                         if ([mur[0]-1, mur[1]] not in murs):
                             murs.append([mur[0]-1, mur[1]])
                             
@@ -157,24 +155,24 @@ def creer_lab(lignes, colonnes):
                 
     
         if mur[0] != lignes -1:
-            if lab[mur[0]-1][mur[1]] == 0 and  lab[mur[0]+1][mur[1]] == 4:
-                nb_chemins = chemins_voisins(mur)
+            if lab[mur[0]-1][mur[1]] == CHEMIN and  lab[mur[0]+1][mur[1]] == NI_NI:
+                nb_chemins = chemins_voisins(mur,lab)
                 if nb_chemins < 2:
-                    lab[mur[0]][mur[1]] = 0
+                    lab[mur[0]][mur[1]] = CHEMIN
                     
                     if (mur[0] != lignes-1):
-                        if (lab[mur[0]+1][mur[1]] != 0):
-                            lab[mur[0]+1][mur[1]] = 1
+                        if (lab[mur[0]+1][mur[1]] != CHEMIN):
+                            lab[mur[0]+1][mur[1]] = MUR
                         if ([mur[0]+1, mur[1]] not in murs):
                             murs.append([mur[0]+1, mur[1]])
-                    if (mur[1] != 0):
-                        if (lab[mur[0]][mur[1]-1] != 1):
-                            lab[mur[0]][mur[1]-1] = 0
+                    if (mur[1] != CHEMIN):
+                        if (lab[mur[0]][mur[1]-1] != MUR):
+                            lab[mur[0]][mur[1]-1] = CHEMIN
                         if ([mur[0], mur[1]-1] not in murs):
                             murs.append([mur[0], mur[1]-1])
                     if (mur[1] != colonnes-1):
-                        if (lab[mur[0]][mur[1]+1] != 0):
-                            lab[mur[0]][mur[1]+1] = 1
+                        if (lab[mur[0]][mur[1]+1] != CHEMIN):
+                            lab[mur[0]][mur[1]+1] = MUR
                         if ([mur[0], mur[1]+1] not in murs):
                             murs.append([mur[0], mur[1]+1])
                     
@@ -184,41 +182,48 @@ def creer_lab(lignes, colonnes):
         delete_wall(mur)
                 
     
-    make_murs(lignes, colonnes)
-    entree_sortie(lignes, colonnes)
+    make_murs(lignes, colonnes,lab)
+    entree_sortie(lignes, colonnes,lab)
+    return lab
 
 
-lignes = 30
-colonnes = 40
-lab = lab0(lignes, colonnes)
-creer_lab(lignes, colonnes)
+#lignes = 100
+#colonnes = 100
+def laby(l,c):
+    lab = creer_lab(l, c)
 
-l,c= lab.shape
+    for i in range(l):
+        if lab[i][0] != ENTREE and lab[i][0] != SORTIE:
+            lab[i][0] = MUR
+        if lab[i][c-1] != ENTREE and lab[i][c-1] != SORTIE:
+            lab[i][c-1] =MUR
+    for j in range(c):
+        if lab[0][j] != ENTREE and lab[0][j] != SORTIE:
+            lab[0][j] = MUR
+        if lab[l-1][j] != ENTREE and lab[l-1][j] != SORTIE:
+            lab[l-1][j] = MUR
+    #print(lab)
+    return lab
 
 
 
+if __name__ == "__main__":
+    lab = laby(50,50)
+    maze_converter.graphics(lab)
+    Nodes = maze_converter.node_inventory(lab)[0]
 
+    Chemin, Distance = parcoursmono.dijkstra_mono(Nodes)
 
-for i in range(l):
-    if lab[i][0] != 2 and lab[i][0] != 3:
-        lab[i][0] = 1
-    if lab[i][c-1] != 2 and lab[i][c-1] != 3:
-        lab[i][c-1] =1
-for j in range(c):
-    if lab[0][j] != 2 and lab[0][j] != 3:
-        lab[0][j] = 1
-    if lab[l-1][j] != 2 and lab[l-1][j] != 3:
-        lab[l-1][j] = 1
+    maze_converter.update_final(lab,Chemin)
+    maze_converter.graphics(lab)
 
+    lignes, colonnes = lab.shape
+    f = open("lab.txt", 'w')
+    for i in range(lignes):
+        for j in range(colonnes):
+            f.write(str(lab[i][j]))
+        f.write('\n')
     
-        
+    f.close()
 
-print(lab)
 
-f = open("lab.txt", 'w')
-for i in range(lignes):
-    for j in range(colonnes):
-        f.write(str(lab[i][j]))
-    f.write('\n')
-
-f.close()
