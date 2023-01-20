3f60df479e8e7c663621ce995ef801e77e800f6d
labidimer <118855567+labidimer@users.noreply.github.com>
2023-01-20 17:44:56 +0100

    Update fct_def_entree_sortie.py
    compare

--- fct_def_entree_sortie.py
+++ fct_def_entree_sortie.py
@@ -12,3 +12,25 @@ def definition_entree_sortie (nodes) :
             if nodes[node].exit :
                id_sortie = node
         return id_entree , id_sortie
+
+def compare(reverse , voisin_id ,node , nodes , heap , heap_reverse=[]) :
+    if not reverse  :
+        heap_choisie = heap 
+        node_dist = node.dist
+    else :
+        heap_choisie= heap_reverse 
+        node_dist = node.reverse_dist
+    voisin_dist=node.neightboors_dist[voisin_id]
+    if not nodes[voisin_id].closed :
+               dist = node_dist + voisin_dist
+               v = nodes[ voisin_id ]
+               v_pred = v.pred if not reverse else v.reverse_pred 
+               v_dist = v.dist if not reverse else v.reverse_dist
+               if v_dist > dist :
+                v_dist = dist
+                v_pred = node.id
+                if v_pred is None :
+                    pimped_heap.heappush( heap_choisie , v )
+                 
+                else :
+                    pimped_heap.decrease_key(heap_choisie , v )
